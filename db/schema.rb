012f66c20e1a20217fbbca915f1f25962aa95994
Jack Moran <jvckmorvn@gmail.com>
2022-06-07 12:13:02 +0100

    Added day_date column to Day model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_07_104026) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_07_111229) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,6 +55,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_07_104026) do
     t.bigint "event_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "day_date"
     t.index ["event_id"], name: "index_days_on_event_id"
   end
 
