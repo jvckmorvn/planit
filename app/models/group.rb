b064a86b016f0b13c6219b438c804669dda9f5f7
Jack Moran <jvckmorvn@gmail.com>
2022-06-07 11:42:14 +0100

    Changed default value for 'locked' column in Event model from true to false

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,4 @@
 class Group < ApplicationRecord
   has_one_attached :picture
+  has_many :users, through: :user_group
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_07_102621) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_07_104026) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,7 +61,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_07_102621) do
   create_table "events", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "group_id", null: false
-    t.boolean "locked"
+    t.boolean "locked", default: false
     t.string "name"
     t.string "colour"
     t.date "locked_start"
