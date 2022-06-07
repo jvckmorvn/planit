69f41697af32f2baf5a341b77ad2b894dbd5c30b
Jack Moran <jvckmorvn@gmail.com>
2022-06-07 17:41:22 +0100

    More work

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "open-uri"
+
+file = URI.open("https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg")
+article = Article.new(title: "NES", body: "A great console")
+article.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+article.save
 
 2.times do
-  avatar = Faker
+  avatar =
   User.create(
     username: Faker::Internet.username,
     email: Faker::Internet.email,
