e0562ff7a0013973a79a3f7746bc68578a6619aa
Jack Moran <jvckmorvn@gmail.com>
2022-06-07 15:22:45 +0100

    Fixed navbar issue

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :user
   belongs_to :group
-  has_many :days
+  has_many :days, dependent: :destroy
   has_many :day_votes, through: :days
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,5 @@
 class Group < ApplicationRecord
   has_one_attached :picture
-  belongs_to :user
-  has_many :user_groups
+  has_many :user_groups, dependent: :destroy
   has_many :users, through: :user_groups
 end
