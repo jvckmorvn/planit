fdbdd29c3ed681752c7e68b062b4f977d5853bf3
Jack Moran <jvckmorvn@gmail.com>
2022-06-09 13:28:01 +0100

    Added buttons to event form

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,4 +1,7 @@
 class Day < ApplicationRecord
   belongs_to :event
   has_many :day_votes, dependent: :destroy
+
+  def change_opacity
+  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,6 @@ class Event < ApplicationRecord
   has_many :day_votes, through: :days
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+  validates :colour, uniqueness: { scope: :group_id, message: "This colour has already been taken" }
+  COLOURS = ["red", "green", "blue", "yellow", "grey"]
 end
