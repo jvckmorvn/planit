ba3256bf5b0e26bd869810698a5857e4de58ebd2
Jack Moran <jvckmorvn@gmail.com>
2022-06-09 15:06:50 +0100

    Reformatted groups index into partials

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,6 @@
 class Group < ApplicationRecord
   has_one_attached :picture
   has_many :user_groups, dependent: :destroy
+  has_many :events, dependent: :destroy
   has_many :users, through: :user_groups
 end
