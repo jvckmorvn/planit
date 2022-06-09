eddaac1ec190f0caa347a0cd1241f11b4aa4e8f5
Jack Moran <jvckmorvn@gmail.com>
2022-06-09 21:53:26 +0100

    Unsaved changes from today

--- app/models/day.rb
+++ app/models/day.rb
@@ -3,5 +3,14 @@ class Day < ApplicationRecord
   has_many :day_votes, dependent: :destroy
 
   def change_opacity
+    group = self.event.group
+    colour = self.event.colour
+    user_ids = UserGroup.where(group: group).distinct.pluck(:user_id)
+    user_count = user_ids.count
+    p user_count
+    # for each day in event, find number of votes
+    # divide total no. of day_votes by user_count
+    # round result to nearest tenth
+    # use result to set opacity value in style attribute of div containing each day
   end
 end
