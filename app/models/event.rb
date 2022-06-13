3385fd8377728f2e0f43175b23eb1506e775645f
Jack Moran <jvckmorvn@gmail.com>
2022-06-13 17:18:59 +0100

    removing redundant stimulus work

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,8 +5,12 @@ class Event < ApplicationRecord
   has_many :day_votes, through: :days
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
-  validates :colour, uniqueness: { scope: :group_id, message: "This colour has already been taken." }
   validates :colour, presence: true
+  validates :colour, uniqueness: { scope: :group_id, message: "This colour has already been taken." }
+  validates :name, presence: true
   validates :name, uniqueness: { scope: :group_id, message: "This name has already been taken." }
+  validates :proposed_start_date, presence: true
+  validates :proposed_end_date, presence: true
+  validates :address, presence: true
   COLOURS = ["maroon", "darkgreen", "midnightblue", "darkorange", "purple"]
 end
