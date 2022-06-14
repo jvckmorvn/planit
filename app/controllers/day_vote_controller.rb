50630165cd569ed285382d0880645219abc99d8a
Jack Moran <jvckmorvn@gmail.com>
2022-06-14 15:17:00 +0100

    All of the work we just did with Louis for event markers and pop-ups

--- app/controllers/day_vote_controller.rb
+++ app/controllers/day_vote_controller.rb
@@ -6,7 +6,7 @@ class DayVoteController < ApplicationController
       unless @day.event.day_votes.where(user: current_user).count > 0
         @vote = DayVote.create(day: @day, user: current_user)
       else
-        flash[:alert] = "Sorry you can only vote for one date."
+        flash[:alert] = "Sorry, you can only vote for one date per event."
       end
       redirect_to group_path(@day.event.group)
   end
