f85933e91f0219029d501b3307cb455e4224fb6c
Boram Lee <boramlee340@gmail.com>
2022-06-14 12:57:47 +0100

    rebuild schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -85,14 +85,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_13_211456) do
     t.string "colour"
   end
 
-  create_table "meetings", force: :cascade do |t|
-    t.string "name"
-    t.datetime "start_time"
-    t.datetime "end_time"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "user_groups", force: :cascade do |t|
     t.bigint "group_id", null: false
     t.bigint "user_id", null: false
