c8073f1d3ae5dd1ddc12d5ade491ad20524a128c
Jack Moran <jvckmorvn@gmail.com>
2022-06-22 01:46:19 +0100

    Ran linter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
-  after_action :redirect, only: {registrations: [:update]}
+  after_action :redirect, only: { registrations: [:update] }
 
   def configure_permitted_parameters
     # For sign-up
--- app/controllers/day_vote_controller.rb
+++ app/controllers/day_vote_controller.rb
@@ -3,17 +3,17 @@ class DayVoteController < ApplicationController
 
   def create
     @day = Day.find(params[:day_id])
-      unless @day.event.day_votes.where(user: current_user).count > 0
-        @vote = DayVote.create(day: @day, user: current_user)
-      else
-        flash[:alert] = "Sorry, you can only vote for one date per event."
-      end
-      redirect_to group_path(@day.event.group)
+    if @day.event.day_votes.where(user: current_user).count.positive?
+      flash[:alert] = "Sorry, you can only vote for one date per event."
+    else
+      @vote = DayVote.create(day: @day, user: current_user)
+    end
+    redirect_to group_path(@day.event.group)
   end
 
   def destroy
     @vote = DayVote.find(params[:id])
-      @vote.destroy
-      redirect_to group_path(@vote.day.event.group), status: :see_other
+    @vote.destroy
+    redirect_to group_path(@vote.day.event.group), status: :see_other
   end
 end
